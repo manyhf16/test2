2372845b59e49d6b217e0f4091a5074e9f907118
zhang <zhang@163.com>
2021-02-07 21:13:15 +0800

    c0

---
+++ src/main/java/com/itheima/test2/Test2Application.java
@@ -0,0 +1,16 @@
+package com.itheima.test2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Test2Application.class, args);
+    }
+
+    static class MyController{
+
+    }
+}
